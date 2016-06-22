381479037709b8de7fe114fecfaae01c84c086f3
jimmcgaw <jpmcgaw@gmail.com>
2016-06-21 22:35:04 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Face Detection in JavaScript
+
+Playing around with the tracking.js library that does computer vision in the browser.
+
+Right now there's a dead simple page that captures the user's webcam and detects faces in the captured frames.
+
+## Install and run
+
+``` $ npm install && bower install ```
+
+``` $ node app.js ```
+
+w00t
---
+++ gulpfile.js
@@ -0,0 +1,55 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+var jsFiles = ['*.js', 'src/**/*.js'];
+
+gulp.task('default', ['sass:watch']);
+
+gulp.task('sass', function () {
+  return gulp.src('./public/sass/styles.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./public/css'));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./public/sass/**/*.scss', ['sass', 'inject']);
+});
+
+gulp.task('inject', function(){
+    var wiredep = require('wiredep').stream;
+    var inject = require('gulp-inject');
+
+    var injectSrc = gulp.src(['./public/css/*.css',
+                             './public/js/*.js'], {read: false});
+    var injectOptions = {
+        ignorePath: '/public'
+    };
+
+    var options = {
+        bowerJson: require('./bower.json'),
+        directory: './public/lib',
+        ignorePath: '../public'
+    };
+
+    return gulp.src('./views/*.ejs')
+        .pipe(wiredep(options))
+        .pipe(inject(injectSrc, injectOptions))
+        .pipe(gulp.dest('./views'));
+
+});
+
+gulp.task('serve', ['jshint', 'sass', 'inject'], function(){
+    var options = {
+        script: 'app.js',
+        delayTime: 1,
+        env: {
+            'PORT': 3000
+        },
+        watch: jsFiles
+    };
+
+    return nodemon(options)
+        .on('restart', function(ev){
+            console.log('Restarting...');
+        });
+});
